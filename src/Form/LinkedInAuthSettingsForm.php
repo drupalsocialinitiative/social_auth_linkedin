d1fae551c5030db7e7b2b410207d62535542e76a
Getulio Sánchez <valentin2507@gmail.com>
2019-01-11 03:06:13 -0500

    Issue #3025253: Get callback URL from route

--- src/Form/LinkedInAuthSettingsForm.php
+++ src/Form/LinkedInAuthSettingsForm.php
@@ -7,6 +7,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Path\PathValidatorInterface;
 use Drupal\Core\Routing\RequestContext;
 use Drupal\Core\Routing\RouteProviderInterface;
+use Drupal\Core\Url;
 use Drupal\social_auth\Form\SocialAuthSettingsForm;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -104,7 +105,7 @@ class LinkedInAuthSettingsForm extends SocialAuthSettingsForm {
       '#disabled' => TRUE,
       '#title' => $this->t('Authorized redirect URL'),
       '#description' => $this->t('Copy this value to <em>Authorized Redirect URLs</em> field of your LinkedIn App settings.'),
-      '#default_value' => $GLOBALS['base_url'] . '/user/login/linkedin/callback',
+      '#default_value' => Url::fromRoute('social_auth_linkedin.callback')->setAbsolute()->toString(),
     ];
 
     $form['linkedin_settings']['advanced'] = [
